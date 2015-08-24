a2b23fba6d2e69d50cd1d6eec3f8f05958c48827
untitled-dice <callicles@fea.st>
2015-08-24 17:59:18 -0500

    Add user balance

--- app.js
+++ app.js
@@ -63,6 +63,45 @@ helpers.getHashParams = function() {
   return hashParams;
 };
 
+/**
+ * Decimal adjustment of a number.
+ *
+ * @param {String}  type  The type of adjustment.
+ * @param {Number}  value The number.
+ * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+ * @returns {Number} The adjusted value.
+ */
+helpers.decimalAdjust = function(type, value, exp) {
+  // If the exp is undefined or zero...
+  if (typeof exp === 'undefined' || +exp === 0) {
+    return Math[type](value);
+  }
+  value = +value;
+  exp = +exp;
+  // If the value is not a number or the exp is not an integer...
+  if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+    return NaN;
+  }
+  // Shift
+  value = value.toString().split('e');
+  value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+  // Shift back
+  value = value.toString().split('e');
+  return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+}
+
+helpers.round10 = function(value, exp) {
+  return helpers.decimalAdjust('round', value, exp);
+};
+
+helpers.floor10 = function(value, exp) {
+  return helpers.decimalAdjust('floor', value, exp);
+};
+
+helpers.ceil10 = function(value, exp) {
+  return helpers.decimalAdjust('ceil', value, exp);
+};
+
 ////////////////////////////////////////////////////////////
 
 // Manage access_token //////////////////////////////////////
@@ -174,9 +213,11 @@ var MoneyPot = (function() {
 function render() {
 
   if (worldStore.user) {
-    var html = 'Logged in as <code>' + worldStore.user.uname+ '</code>';
-    html += '<button type="button" onclick="onWithdrawClick()" class="btn btn-default btn-xs btn-logout">Withdraw</button>';
+    var html = '';
+    html += '<div class="controls"><button type="button" onclick="onWithdrawClick()" class="btn btn-default btn-xs btn-logout">Withdraw</button>';
     html += '<button type="button" onclick="onLogout()" class="btn btn-default btn-xs btn-logout">Logout</button>';
+    html += '</div>';
+    html += 'Logged in as <code>' + worldStore.user.uname+ '</code> with <code>' + helpers.round10(worldStore.user.balance/100, -2) + '</code> bits' ;
     $('#user-info').html(html);
   } else {
     $('#user-info').empty();
@@ -227,6 +268,7 @@ function onRecaptchaSubmit(response) {
   MoneyPot.claimFaucet(response, {
     success: function(data) {
       console.log('Successful claim:', data);
+      worldStore.user.balance += data.amount;
       worldStore.faucetResult = 'FAUCET_SUCCESS';
       worldStore.claimedAt = new Date();
       localStorage.setItem('claimed_at', (new Date()).toISOString());
